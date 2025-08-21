0037917e2d40c8c6840becf75a98450407076e5a
Hunter Dyar <bloop@hdyar.com>
2025-08-20 21:58:12 -0400

    scene test

--- SceneTest/ScenePanel.cs
+++ SceneTest/ScenePanel.cs
@@ -11,11 +11,11 @@ using static Raylib_cs.Raylib;
 public class ScenePanel
 {
 	private Camera3D camera;
-	private Model mavis;
+	private Model plane;
 	private Vector3 mavisPos;
-	private ModelAnimation mavisWalkAnimation;
-	private int mavisWalkFrameCounter = 0;
-
+	private ModelAnimation[] animations;
+	private int animFrame = 0;
+	private int animCount = 0;
 	private Shader postShader;
 	public RenderTexture2D fightScreenTex;
 	
@@ -35,30 +35,24 @@ public class ScenePanel
 		camera.FovY = 80;
 		camera.Projection = CameraProjection.Perspective;
 		mavisPos = new Vector3(0, 0, 0);
-		var fileInfo = new FileInfo("Resources/models/sceneTest.glb");
-		 fileInfo = new FileInfo("Resources/models/scenetest/sceneTest.glb");
+		var fileInfo = new FileInfo("Resources/models/scenetest/planepop.glb");
 
 		if (fileInfo.Exists)
 		{
-			mavis = LoadModel(fileInfo.FullName);
+			plane = LoadModel(fileInfo.FullName);
 		}
 		else
 		{
 			throw new Exception("Model not found");
 		}
 
-		BoundingBox bounds = GetMeshBoundingBox(mavis.Meshes[0]);
+		//BoundingBox bounds = GetMeshBoundingBox(mavis.Meshes[0]);
 
-
-		int animCount = 0;                                               
-		var anims = LoadModelAnimations("Resources/models/sceneTest.glb",ref animCount);
-		if (animCount > 0)
+		var anims = LoadModelAnimations("Resources/models/scenetest/planepop.glb",ref animCount);
+		animations = new ModelAnimation[animCount];
+		for (int i = 0; i < animCount; i++)
 		{
-			mavisWalkAnimation = anims[0]; //we ... assume!
-		}
-		else
-		{
-			//UHH
+			animations[i] = anims[i]; //we ... assume!
 		}
 	}
 
@@ -75,31 +69,35 @@ public class ScenePanel
 		//rotate camera slowly
 		camera.Position = new  Vector3(MathF.Sin((float)Raylib.GetTime()/8f)*10, 5, MathF.Sin(Single.Pi/2+(float)Raylib.GetTime() / 8f) * 10);
 		//Loop walk Animation.
-		mavisWalkFrameCounter++;
-		//UpdateModelAnimation(mavis,mavisWalkAnimation, mavisWalkFrameCounter);
-		 if (mavisWalkFrameCounter >= mavisWalkAnimation.FrameCount)
-		 {
-		 	mavisWalkFrameCounter = 0;
-		 }
-		
-		
+		// animFrame++;
+		// if (animFrame >= animations[0].FrameCount)
+		// {
+		// 	animFrame = 0;
+		// }
+		// for (int i = 0; i < animations.Length; i++)
+		// {
+		// 	UpdateModelAnimation(plane, animations[i], 0);
+		// }
+
+		UpdateModelAnimation(plane, animations[0], 0);
+
+
+
 		BeginTextureMode(fightScreenTex);
 		
 			ClearBackground(Color.Black);
-			//DrawRectangle(PosX, PosY, Width, Height, Color.Black);
 			DrawCircle(GetScreenWidth()/2, GetScreenHeight()/2, 400,Color.Red);
 			BeginMode3D(camera);
 				//DrawGrid(10, 1.0f);
 				
 				DrawModelEx(
-					mavis,
+					plane,
 					new Vector3(0, 0, 0),
 					new Vector3(0.0f, 1.0f, 0.0f), 
 					0.0f,
 					new Vector3(1f, 1f, 1f),
 					Color.White
 				);
-				
 			EndMode3D();
 		 EndTextureMode();
 		
@@ -107,14 +105,18 @@ public class ScenePanel
 			DrawTextureRec(fightScreenTex.Texture,
 			new Rectangle(0, 0, (float)fightScreenTex.Texture.Width, (float)-fightScreenTex.Texture.Height), new Vector2(0, 0), Color.White);
 			EndShaderMode();
-		
+		DrawFPS(0,0);
 	}
 
 	public void OnClose()
 	{
 		UnloadRenderTexture(fightScreenTex);
 		UnloadShader(postShader);
-		UnloadModelAnimation(mavisWalkAnimation);
-		UnloadModel(mavis);
+		foreach (var animation in animations)
+		{
+			UnloadModelAnimation(animation);
+
+		}
+		UnloadModel(plane);
 	}
 }
