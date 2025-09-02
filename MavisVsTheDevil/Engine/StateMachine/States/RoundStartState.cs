78a5e981f5a50d2fb5b7617c5f1a51e3c11d1934
Hunter Dyar <bloop@hdyar.com>
2025-09-02 13:59:53 -0400

    Cause Of Death Banner

--- MavisVsTheDevil/Engine/StateMachine/States/RoundStartState.cs
+++ MavisVsTheDevil/Engine/StateMachine/States/RoundStartState.cs
@@ -10,6 +10,7 @@ public class RoundStartState : StateBase
 	private readonly Scene _scene;
 	private string _modifiers = "";
 	private BannerText _demonNameBanner;
+	private BannerText _causeOfDeathBanner;
 	private BannerText _wordListBanner;
 	private BannerText _modifierBanner;
 	public RoundStartState(StateMachine machine) : base(machine)
@@ -31,19 +32,21 @@ public class RoundStartState : StateBase
 			"You meet the demon", false, _machine.Game.CurrentRound.Demon.Name);
 		//, $"Cause of death: {_machine.Game.CurrentRound.Demon.CauseOfDeath}
 
+		_causeOfDeathBanner = new BannerText(bannerHeight, bannerHeight * 2 + bannerGap, "Cause Of Death", false,
+			_machine.Game.CurrentRound.Demon.CauseOfDeath);
 		
-		_wordListBanner = new BannerText(bannerHeight, bannerHeight*2+bannerGap,
+		_wordListBanner = new BannerText(bannerHeight, bannerHeight*3+bannerGap*2,
 			"Word List", false, _machine.Game.CurrentRound.WordlistName);
 		
 		//modifier banner
 		_modifiers = ModifierUtility.GetModifierNames(_machine.Game.CurrentRound.Test.Modifiers);
 		if (_machine.Game.CurrentRound.Test.Modifiers.Length == 0)
 		{
-			_modifierBanner = new BannerText(bannerHeight, bannerHeight*3+bannerGap*2, "Modifiers:",false, "None");
+			_modifierBanner = new BannerText(bannerHeight, bannerHeight*4+bannerGap*3, "Modifiers:",false, "None");
 		}
 		else
 		{
-			_modifierBanner = new BannerText(bannerHeight, bannerHeight*3+bannerGap*2, "Modifiers:",false, _machine.Game.CurrentRound.Test.Modifiers.Select(x=>x.GetModifierName()).ToArray());
+			_modifierBanner = new BannerText(bannerHeight, bannerHeight*4+bannerGap*3, "Modifiers:",false, _machine.Game.CurrentRound.Test.Modifiers.Select(x=>x.GetModifierName()).ToArray());
 		}
 
 		Program.GameWindow.TypingWindow.SetTextOpacity(0.5f);
@@ -55,6 +58,12 @@ public class RoundStartState : StateBase
 		if (!_demonNameBanner.IsComplete)
 		{
 			_demonNameBanner.Tick(delta);
+			return;
+		}
+
+		if (!_causeOfDeathBanner.IsComplete)
+		{
+			_causeOfDeathBanner.Tick(delta);
 			return;
 		}
 
@@ -75,20 +84,29 @@ public class RoundStartState : StateBase
 			_modifierBanner.ExitTween.Tick(delta);
 		}
 
-		if (_modifierBanner.ExitTween.PercentageComplete > 0.5f && !_wordListBanner.ExitTween.IsComplete)
+		
+		if (_modifierBanner.ExitTween.PercentageComplete > 0.5f &&
+		    !_wordListBanner.ExitTween.IsComplete)
 		{
 			_wordListBanner.ExitTween.Tick(delta);
 		}
+
+		if (_modifierBanner.ExitTween.PercentageComplete > 0.5f &&
+		    _wordListBanner.ExitTween.PercentageComplete > 0.5f &&
+		    !_causeOfDeathBanner.ExitTween.IsComplete)
+		{
+			_causeOfDeathBanner.ExitTween.Tick(delta);
+		}
 		
-		if (_modifierBanner.ExitTween.PercentageComplete > 0.5f && _wordListBanner.ExitTween.PercentageComplete > 0.5f && !_demonNameBanner.ExitTween.IsComplete)
+		if (_modifierBanner.ExitTween.PercentageComplete > 0.5f &&
+		    _causeOfDeathBanner.ExitTween.PercentageComplete > 0.5f &&
+		    _wordListBanner.ExitTween.PercentageComplete > 0.5f && !_demonNameBanner.ExitTween.IsComplete)
 		{
 			_demonNameBanner.ExitTween.Tick(delta);
 		}
-
-		
 		
 		//done
-		if (_modifierBanner.IsComplete && _wordListBanner.ExitTween.IsComplete && _demonNameBanner.ExitTween.IsComplete)
+		if (_modifierBanner.IsComplete && _wordListBanner.ExitTween.IsComplete && _demonNameBanner.ExitTween.IsComplete && _causeOfDeathBanner.ExitTween.IsComplete)
 		{
 			_machine.GoToState(_machine.TypeGameplay);
 		}
@@ -106,6 +124,7 @@ public class RoundStartState : StateBase
 	public override void Draw()
 	{
 		_demonNameBanner.Draw();
+		_causeOfDeathBanner.Draw();
 		_wordListBanner.Draw();
 		_modifierBanner.Draw();
 		var demon = _machine.Game.CurrentRound?.Demon;
